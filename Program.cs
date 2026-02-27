ce25a0fbd91d81e836fa08ec7799b7bb87aac78f
rohanbirgonda23 <rohanbirgonda23@gmail.com>
2026-02-27 09:05:12 +0530

    Added reverse number program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        int rev = 0;
+
+        while(num != 0)
+        {
+            int dig = num%10;
+            rev = rev * 10 + dig;
+            num = num/10;
+        }
+
+        Console.WriteLine("Reversed Number: " + rev);
+    }
+}
